e370302ce5b296c21a44c22c0a795d96d072a9c1
chrisyt3718 <108691729+chrisyt3718@users.noreply.github.com>
2022-07-07 09:14:08 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    count += 1
+
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+    return render_template("home.html", count=count)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/Happy_Siu")
+def Happy_Siu():
+    return "Welcome to Happy Siu's Website: "
+
+if __name__ == "__main__":
+    app.run()
